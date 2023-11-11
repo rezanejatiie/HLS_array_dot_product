d8ab432e5540dc96af2b8699d3fbfe9e428212d2
rezanejati2 <rezanejatti@gmail.com>
2023-11-12 00:50:06 +0330

    first

--- source/hw_array_dot_pr.cpp
+++ source/hw_array_dot_pr.cpp
@@ -4,18 +4,6 @@ void hw_dot_product(int a[N], int b[N] , int &out)
 
 {
 
-//	int a_int[N],b_int[N];
-//	#pragma HLS array_partition  variable=a_int dim=1 complete
-//	#pragma HLS array_partition  variable=b_int dim=1 complete
-//
-//	  for(int i=0;i<N;i++) {
-//	  #pragma HLS pipeline
-//	    a_int[i] = a[i];
-//	  }
-//	  for(int i=0;i<N;i++) {
-//	  #pragma HLS pipeline
-//	    b_int[i] = b[i];
-//	  }
 
 	  // vector dot products
 	  int product = 0;
